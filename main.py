cf7ef5e90abf017b07c22b034e234bafaf5d845b
villerbond <91826226+villerbond@users.noreply.github.com>
2023-05-27 13:20:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ onc = pd.read_excel('Data_exam.xlsx', header=1)
 pd.set_option('display.max_columns', None)
 
 print(onc.head())
-
-g = sns.FacetGrid(data = onc, col=2)
-g.map(sns.histplot(onc, x="Возраст, лет"))
-g.map(sns.histplot(onc, x="Пол"))
+sns.histplot(onc, x="Возраст, лет")
 
 plt.show()
