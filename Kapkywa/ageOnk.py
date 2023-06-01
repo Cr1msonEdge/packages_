9616f769020f1a9ae6225597d357abbe8cc82d9c
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2023-06-01 11:30:37 +0300

    Delete Kapkywa directory

--- Kapkywa/ageOnk.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-df = pd.read_excel("Data_exam.xlsx", header=1)
-#sns.histplot(data=df, x="Возраст, лет", hue="Онк/Неонк", bins=10, multiple="dodge", shrink=0.8)
-sns.histplot(data=df, x="Возраст, лет", hue="Онк/Неонк", bins=20)
-plt.show()
